8df702a9afadb1c445cdbefe7ac1b60e37a9b0ea
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-08-21 16:56:31 +0530

    Updated CORS configuration

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from "express";
-const PORT = process.env.PORT || 3009;
+import cors from "cors";
 import connection from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import noteRouter from "./routes/note.route.js";
 import auth from "./middleware/auth.middleware.js";
-import cors from "cors";
 
+const PORT = process.env.PORT || 3009;
 const server = express();
+
+server.use(cors({
+    origin: "*"
+}));
 
 server.use(express.json());
 server.use("/user", userRouter)
 server.use("/note", auth, noteRouter)
-server.use(cors());
+
 
 server.get("/", (req, res)=>{
     res.send("Hello World")
