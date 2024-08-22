66e76291d94dacde0ecb26e1fcabcb039dbdc537
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-08-23 04:54:34 +0530

    Fix Get

--- routes/note.route.js
+++ routes/note.route.js
@@ -23,46 +23,10 @@ noteRouter.post("/create", async (req, res) => {
 
 noteRouter.get("/", async (req, res) => {
   const userId = req.user._id;
-  const { noteId, title, page = 1, limit = 2, sort = "createdAt" } = req.query;
 
   try {
-    let query = { userId };
-
-    //Filter by notesId if provided
-
-    if (noteId) {
-      const note = await noteModel.findOne({ _id: noteId, userId });
-      if (!note) {
-        return res.status(404).json({ msg: "Note not found" });
-      }
-      return res
-        .status(200)
-        .json({ msg: `Found note with ID: ${noteId}`, note });
-    }
-
-    //Filter by title
-
-    if (title) {
-      query.title = new RegExp(title, "i");
-    }
-
-    const notes = await noteModel
-      .find(query)
-      .limit(limit)
-      .skip((page - 1) * limit)
-      .exec();
-
-    const totalNotes = await noteModel.countDocuments(query);
-
-    res
-      .status(200)
-      .json({
-        msg: `${req.user.userName} all notes`,
-        notes,
-        totalNotes,
-        currentPage: page,
-        totalPages: Math.ceil(totalNotes/limit)
-      });
+    const notes = await noteModel.find({ userId });
+    res.status(200).json({ msg: `${req.user.userName} all notes`, notes });
   } catch (error) {
     res.status(500).json({ msg: `Error in finding notes ${error}` });
   }
@@ -106,5 +70,6 @@ noteRouter.delete("/delete/:id", async (req, res) => {
       .json({ msg: `Error occured while deleting note, ${error}` });
   }
 });
+
 
 export default noteRouter;
